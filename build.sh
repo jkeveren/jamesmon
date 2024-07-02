5c559c3cc171775e33497b8eeba1d50b494e0afc
James Keveren <james@keve.ren>
2024-07-02 17:39:00 +0100

    comments

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#https://github.com/jkeveren/jamesmon
 cromple \
 -g \
 -std=c++20 \
+-static \
 -Wfatal-errors -Werror \
 -Wall -Wextra -Wpedantic \
 -Wfloat-equal -Wsign-conversion -Wfloat-conversion \
@@ -14,3 +16,5 @@ cromple \
 -Wno-error=unused-variable \
 -Wno-error=unused-value \
 -o bin/jamesmon
+
+# sudo setcap "cap_perfmon=pe" bin/jamesmon
